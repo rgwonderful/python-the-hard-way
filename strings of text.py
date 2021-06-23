8783ad9064f0d2967678c16359d55c420901d6ff
Rex-Goliath Wonderful <rg.teamwonderful@gmail.com>
2021-06-23 11:25:49 -0500

    finished exercise 6

---
+++ strings of text.py
@@ -0,0 +1,22 @@
+types_of_people = 10
+x = f"There are {types_of_people} types of people."
+
+binary = "binary"
+do_not = "don't"
+y = f"those who know {binary} and those who {do_not}."
+
+print(x)
+print(y)
+# odd taxi ed
+print(f"i said: {x}")
+print(f"i also said '{y}'")
+
+hilarious = False
+joke_evaluation = "isn't that joke so funny?! {}"
+
+print(joke_evaluation.format(hilarious))
+
+w = "this is the left side of..."
+e = "a string with a right side"
+
+print(w + e)
