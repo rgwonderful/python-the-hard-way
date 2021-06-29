c3bc543e83735077b89cbae201a815d5ae3900b4
Rex-Goliath Wonderful <rg.teamwonderful@gmail.com>
2021-06-29 11:50:23 -0500

    finished exersie 9

---
+++ li printing,printing,printing.py
@@ -0,0 +1,14 @@
+#heres some ne strange stuff, remember to type what i type exatly
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("here are the days: ", days)
+print("here are the months: ", months)
+
+print("""
+thers somthing going on here.
+with the three double-quotes.
+we'll be able to type as much as we like.
+even 4 lines if we want, or 5, or 6.
+""")
